d18dadde3db88a040a47e68be3a326da464f3eb8
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-12-08 11:33:05 -0700

    Added endpoint to retrieve individual action by ID

--- actionsRouter.js
+++ actionsRouter.js
@@ -7,7 +7,7 @@ const express = require('express');
 
 const router = express.Router();
 
-// respond with full array of projects
+// respond with full array of actions
 
 router.get('/', (req, res) => {
     actionsDb.get()
@@ -19,6 +19,28 @@ router.get('/', (req, res) => {
             res
                 .status(500)
                 .json({ message: `The actions' information could not be retrieved.`});
+        });
+});
+
+// respond with individual action
+ 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    actionsDb.get(id)
+        .then(action => {
+            if (action) {
+                res.json(action);
+            }
+            else {
+                res
+                    .status(404)
+                    .json({ message: 'The action you requested does not exist.'});
+            }
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: `The action's information could not be retrieved.`});
         });
 });
 
