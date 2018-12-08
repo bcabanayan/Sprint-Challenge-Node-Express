f02f9610666a5f91dc8d8438eef81248957f009d
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-12-08 11:31:35 -0700

    Added endpoint to get full list of actions

--- actionsRouter.js
+++ actionsRouter.js
@@ -1,11 +1,26 @@
 // dependencies
 
-const projectsDb = require('./data/helpers/actionModel.js')
+const actionsDb = require('./data/helpers/actionModel.js')
 const express = require('express');
 
 // define router
 
 const router = express.Router();
+
+// respond with full array of projects
+
+router.get('/', (req, res) => {
+    actionsDb.get()
+        .then(actions => {
+            res
+                .json(actions);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: `The actions' information could not be retrieved.`});
+        });
+});
 
 // export router
 
